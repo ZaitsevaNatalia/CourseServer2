cdbf33f72a0c0390692979752cb71903903d4274
Natalia Zaytseva <zajanata@mail.ru>
2017-03-11 06:51:02 +0200

    Update

--- softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/Request.java
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/Request.java
@@ -1,7 +1,11 @@
 package com.softgroup.common.protocol;
 
+
+
 import java.io.Serializable;
 
 public class Request<T extends Serializable> extends RoutedAction<T> {
 	private static final long serialVersionUID = -1024761631293962969L;
 }
+
+
